8050f4551f4d8211e2c8f1e21068e0960fa532ee
Manase <manasesmutembei11@gmail.com>
2024-07-24 12:07:36 +0300

    initial

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,10 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [],
   templateUrl: './home.component.html',
-  styles: []
+  styleUrl: './styles.css'
 })
 export class HomeComponent {
 
