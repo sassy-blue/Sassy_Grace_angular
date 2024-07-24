1d597efa20f0772d50433401220f7482547aa579
Manase <manasesmutembei11@gmail.com>
2024-07-24 13:04:25 +0300

    portfolio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
