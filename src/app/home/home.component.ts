3a71691b713cc6581a5b4c4b994b2e1fde62b082
Manase <manasesmutembei11@gmail.com>
2024-07-29 16:58:05 +0300

    News component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { ContactComponent } from './components/contact/contact.component';
 import { ProjectComponent } from "./components/project/project.component";
+import { NewsListComponent } from "./components/news-list/news-list.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   standalone: true,
-  imports: [ContactComponent, ProjectComponent]
+  imports: [ContactComponent, ProjectComponent, NewsListComponent]
 })
 export class HomeComponent {
 
